0789382cdaef2f403ac4b643bdb33427913e899b
ihnatovcharenko25-gif <ihnat.ovcharenko.25@kse.org.ua>
2026-03-15 21:07:56 +0200

    MyStack full, bugfixed

---
+++ ShuntingYard_Ovcharenko/Stack.cs
@@ -0,0 +1,31 @@
+﻿public class MyStack
+{
+    private const int Capacity = 50;
+    private string[] _array = new string[Capacity];
+    private int _pointer;
+    
+    public void Push(string value)
+    {
+        if (_pointer == _array.Length)
+        {
+            throw new Exception("Stack overflowed");
+        }
+        _array[_pointer] = value;
+        _pointer++;
+    }
+    
+    public string Pop()
+    {
+        if (_pointer == 0)
+            return null;
+        var value = _array[_pointer - 1];
+        _pointer--;
+        return value;
+    }
+    
+    public string Examine()
+    {
+        if (_pointer == 0) return null;
+        return _array[_pointer - 1];
+    }
+}
