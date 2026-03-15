06423dc9adca4d43a1bf49f8b7e7d39a98f305d1
ihnatovcharenko25-gif <ihnat.ovcharenko.25@kse.org.ua>
2026-03-15 21:27:28 +0200

    Queue with cycle

---
+++ ShuntingYard_Ovcharenko/Queue.cs
@@ -0,0 +1,34 @@
+﻿public class MyQueue
+{
+    private const int Capacity = 50;
+    private string[] _array = new string[Capacity];
+    private int _pointer, _pointerBegin;
+    private int _count;
+    
+    public void Enqueue(string value)
+    {
+        if (_count == Capacity)
+        {
+            throw new Exception("Queue overflowed");
+        }
+        _array[_pointer] = value;
+        _pointer++; _pointer %= Capacity;
+        _count++;
+    }
+    
+    public string Dequeue()
+    {
+        if (_count == 0)
+            return null;
+        var value = _array[_pointerBegin];
+        _pointerBegin++; _pointerBegin %= Capacity;
+        _count--;
+        return value;
+    }
+    
+    public string Examine()
+    {
+        if (_count == 0) return null;
+        return _array[_pointerBegin];
+    }
+}
